d041f1685555f2275e22436fbfbc202a2689f63e
Thomas Witzel <witzel.thomas@gmail.com>
2021-11-30 18:59:23 +0000

    basic script outlining how to patch the created yocto image with the outputs from the xenomai build to create a xenomai kerneled version of our boot image

---
+++ scripts/patch_yocto_image.sh
@@ -0,0 +1,12 @@
+# the whole thing needs to be run as root
+mkdir /media/rp_boot
+mkdir /media/rp_rootfs
+kpartx_result=$(kpartx -av redpitaya_openmri_yocto_xenomai_v2_11302021.img)
+read img_boot_dev img_root_dev <<<$(grep -o 'loop[0-9]*p[0-9]' <<<"$kpartx_result")
+mount -o rw -t vfat /dev/mapper/$img_boot_dev /media/rp_boot
+mount -o rw -t ext4 /dev/mapper/$img_root_dev /media/rp_rootfs
+cp Xenomai/linux-xeno-build/arch/arm/boot/uImage /media/rp_boot/
+rsync -arv Xenomai/xeno3_zynq_stage/* /media/rp_rootfs/
+rsync -arv Xenomai/zynq_modules/* /media/rp_rootfs/
+umount /media/rp_boot
+umount /media/rp_rootfs
