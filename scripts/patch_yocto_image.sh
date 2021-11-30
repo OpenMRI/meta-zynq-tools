31718705c04ae504c4209873c083e4de8886740a
Thomas Witzel <witzel.thomas@gmail.com>
2021-11-30 19:26:11 +0000

    fixed some of the grep and read stuff, added clean removal of the mapped devices

--- scripts/patch_yocto_image.sh
+++ scripts/patch_yocto_image.sh
@@ -2,7 +2,7 @@
 mkdir /media/rp_boot
 mkdir /media/rp_rootfs
 kpartx_result=$(kpartx -av redpitaya_openmri_yocto_xenomai_v2_11302021.img)
-read img_boot_dev img_root_dev <<<$(grep -o 'loop[0-9]*p[0-9]' <<<"$kpartx_result")
+read -d \n img_boot_dev img_root_dev <<<$(grep -o 'loop[0-9]*p[0-9]' <<<"$kpartx_result")
 mount -o rw -t vfat /dev/mapper/$img_boot_dev /media/rp_boot
 mount -o rw -t ext4 /dev/mapper/$img_root_dev /media/rp_rootfs
 cp Xenomai/linux-xeno-build/arch/arm/boot/uImage /media/rp_boot/
@@ -10,3 +10,7 @@ rsync -arv Xenomai/xeno3_zynq_stage/* /media/rp_rootfs/
 rsync -arv Xenomai/zynq_modules/* /media/rp_rootfs/
 umount /media/rp_boot
 umount /media/rp_rootfs
+# remove the mapping again
+kpartx -d redpitaya_openmri_yocto_xenomai_v2_11302021.img
+dmsetup remove -f /dev/mapper/$img_boot_dev
+dmsetup remove -f /dev/mapper/$img_root_dev
