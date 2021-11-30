0e2f54f54cd9246cfe99b7a1ff2ef36f4cf78b01
Thomas Witzel <witzel.thomas@gmail.com>
2021-11-30 19:26:57 +0000

    added synchronous mode for kpartx to make sure devices are mapped properly before trying to mount them

--- scripts/patch_yocto_image.sh
+++ scripts/patch_yocto_image.sh
@@ -1,7 +1,7 @@
 # the whole thing needs to be run as root
 mkdir /media/rp_boot
 mkdir /media/rp_rootfs
-kpartx_result=$(kpartx -av redpitaya_openmri_yocto_xenomai_v2_11302021.img)
+kpartx_result=$(kpartx -sav redpitaya_openmri_yocto_xenomai_v2_11302021.img)
 read -d \n img_boot_dev img_root_dev <<<$(grep -o 'loop[0-9]*p[0-9]' <<<"$kpartx_result")
 mount -o rw -t vfat /dev/mapper/$img_boot_dev /media/rp_boot
 mount -o rw -t ext4 /dev/mapper/$img_root_dev /media/rp_rootfs
